e88c8b7c878ff1393519d484d7b1f6569546ea7a
Uriel Gonzales <urielgonzales2003@gmail.com>
2025-09-17 16:51:41 -0300

    ss

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import adapter.ImpresoraPDF;
 import adapter.ImpresoraTexto;
 import adapter.ImpresoraTextoAdapter;
 import builder.*;
+import composite.ElementoMenu;
+import composite.Menu;
+import composite.Plato;
 import decorator.DescargasOffline;
 import decorator.HD;
 import decorator.Plan;
@@ -46,6 +49,27 @@ public class Main {
         System.out.println("Costo total: $" + plan1.getCosto());
         //Ejercicio 4
 
+        //Ejercicio 7
+        System.out.println("Ejercicio 7");
+        // Platos individuales
+        ElementoMenu plato1 = new Plato("Milanesa con papas", 2500);
+        ElementoMenu plato2 = new Plato("Ensalada César", 1800);
+        ElementoMenu plato3 = new Plato("Pizza Margarita", 2200);
+        ElementoMenu plato4 = new Plato("Helado de vainilla", 900);
+
+        // Submenú de postres
+        Menu postres = new Menu("Postres");
+        postres.agregar(plato4);
+
+        // Menú principal
+        Menu menuPrincipal = new Menu("Menú del Día");
+        menuPrincipal.agregar(plato1);
+        menuPrincipal.agregar(plato2);
+        menuPrincipal.agregar(plato3);
+        menuPrincipal.agregar(postres); // un menú dentro de otro
+
+        // Mostrar todo
+        menuPrincipal.mostrar();
 
 
 
